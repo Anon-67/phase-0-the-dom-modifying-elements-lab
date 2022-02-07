3adfe0cd9661aa6ac34730d51916774f9f3c93db
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-02-07 10:08:24 -0600

    commit tree d82a485c993d0f2d4e881fcf092719cd176ce1a0

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const test = document.getElementById("main");
+
+test.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+let name = "Stefan"
+
+newHeader.textContent = `${name} is the champion`
